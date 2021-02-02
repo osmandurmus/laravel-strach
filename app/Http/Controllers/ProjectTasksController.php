76d916a9930e63645ba766da7422f4a7d237aa3e
odd <deneme123@gmail.com>
2021-02-02 10:08:22 +0300

    better encapsulation first

--- app/Http/Controllers/ProjectTasksController.php
+++ app/Http/Controllers/ProjectTasksController.php
@@ -22,9 +22,11 @@ class ProjectTasksController extends Controller
 
     public function update(Task $task){
         
-        $task->update([
-            'completed' => request()->has('completed')
-        ]);
+        $task->complete(request()->has('completed'));
+        
+        // $task->update([
+        //     'completed' => request()->has('completed')
+        // ]);
 
         return back();
     }
--- app/Task.php
+++ app/Task.php
@@ -8,7 +8,24 @@ class Task extends Model
 {
     protected $guarded = [];
 
+    // improve encapsulation
+    public function complete($completed = true){   // $task->complete(false)
+
+        $this->update(compact('completed')); 
+        // $this->update(['completed' => true]);
+
+    }
+
+    public function incomplete(){
+
+        $this->complete(false);
+        // $this->update(['completed' => false]);
+
+    }
+
     public function project(){
+
         return $this->belongsTo(Project::class);
+
     }
 }
